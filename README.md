b04d58851be6d61c19738a1e5580f549d26deb91
PriyabrataDash-2698 <git@stackblitz.com>
2024-12-25 10:09:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-i5e6bkrs
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/PriyabrataDash-2698/react-i5e6bkrs)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function MyButton() {
+  return <button>click here</button>;
+}
+export default function App() {
+  return (
+    <div>
+      <MyButton />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
